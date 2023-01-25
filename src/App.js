27a3a2943652cb778eefa6d95717a3a110d4f19d
P <dpheeraphat2547@gmail.com>
2023-01-25 11:31:23 +0700

    create state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  {/* ข้อมูลประเทศ */ }
+  const data = [
+    {
+      id: 1, name: "Thailand", region: "Asia", population: 100
+    },
+
+    {
+      id: 2, name: "Canada", region: "Canada", population: 50
+    },
+
+    {
+      id: 3, name: "Canada", region: "japan", population: 150
+    }
+  ]
+
+  const [countries, setCountries] = useState(data);
+
   return (
     <div className="App">
-     
+
+      <div className='container'>
+        <ul>
+        {countries.map((item)=>{
+          return <li key={item.id}>{item.name} | {item.region} | {item.population}</li>
+        })}
+        </ul>
+      </div>
+
     </div>
   );
 }
