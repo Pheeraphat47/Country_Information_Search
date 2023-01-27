6498122f6680bfc0324d439fc0cb75aec36cd871
P <dpheeraphat2547@gmail.com>
2023-01-27 10:53:53 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
 
 function App() {
-
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([])
+  const [word, setWord] = useState("")
+  const [dataFilter] = useState(["name", "capital"])
 
   useEffect(() => {
     fetch("https://restcountries.com/v2/all")
@@ -13,38 +14,55 @@ function App() {
       })
   }, [])
 
-
-
+  const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+  const searchCountries = (countries) => {
+    return countries.filter((item) => {
+      return dataFilter.some((filter) => {
+        if (item[filter]) {
+          return item[filter].toString().toLowerCase().indexOf(word.toLowerCase()) > -1
+        }
+      })
+    })
+  }
   return (
-
-    <div className='container'>
-      <ul className='row'>
-        {countries.map((item, index) => {
+    <div className="container">
+      <div className="search-container">
+        <label htmlFor="search-form">
+          <input type="text"
+            className="search-input"
+            placeholder="Search country that you want ... "
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />         
+        </label>
+      </div>
+      <ul className="row">
+        {searchCountries(countries).map((item, index) => {
           return (
             <li key={index}>
-              <div className='card'>
-                 <div className='card-title'>
-                     <img src={item.flag} alt={item.name}></img>
-                 </div>
-                 <div className='card-body'>
-                     <div className='card-description'> 
-                        <h2>{item.name}</h2>
-                        <ol className='card-list'>
-                          <li>Population : <span>{item.population}</span></li>
-                          <li>Region : <span>{item.region}</span></li>                          
-                          <li>Capital : <span>{item.capital}</span></li>                                                  
-                        </ol>
-                     </div>
-                 </div>
+              <div className="card">
+                <div className="card-title">
+                  <img src={item.flag} alt={item.name} />
+                </div>
+                <div className="card-body">
+                  <div className="card-description">
+                    <h2>{item.name}</h2>
+                    <ol className="card-list">
+                      <li>Population : <span>{formatNumber(item.population)}</span></li>
+                      <li>Region : <span>{item.region}</span></li>
+                      <li>Subregion : <span>{item.subregion}</span></li>
+                      <li>Capital : <span>{item.capital}</span></li>
+                    </ol>
+                  </div>
+                </div>
               </div>
             </li>
-
           )
         })}
       </ul>
     </div>
-
-
   );
 }
 
